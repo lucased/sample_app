236205ed5372c08e1efdb950b3ee67e07c65e82d
Lucas Dobbins <lucasdobbins@gmail.com>
2014-02-17 20:46:30 +1100

    add title to layout + specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+
 	describe "Home Page" do
 
 		it "it shoudld have content 'Sample App" do
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
 		it "should have title 'Home'" do
 			visit '/static_pages/home'
-			expect(page).to have_title('Home')
+			expect(page).to have_title("#{base_title} | Home")
 		end
 	end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
 		it "should have title 'Helo'" do
 			visit '/static_pages/help'
-			expect(page).to have_title('Help')
+			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
@@ -37,7 +39,7 @@ describe "StaticPages" do
 
 		it "should have title 'About'" do
 			visit '/static_pages/about'
-			expect(page).to have_title('About')
+			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
 
