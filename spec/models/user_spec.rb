1baceff9d10b49902549115aa9fbc607b20e439d
Lucas Dobbins <lucasdobbins@gmail.com>
2014-03-06 18:11:28 +1100

    added email case spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,5 +95,15 @@ describe User do
 		it { should_not be_valid }
 	end
 
+	describe "email with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAmPlE.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
 
 end
